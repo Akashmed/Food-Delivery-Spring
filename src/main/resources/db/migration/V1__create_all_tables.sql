205ac419c2b995c78f2ff022e2da1a31600f329d
Akashmed <akashmed3@gmail.com>
2026-06-04 01:21:36 +0600

    create restaurants table

--- src/main/resources/db/migration/V1__create_all_tables.sql
+++ src/main/resources/db/migration/V1__create_all_tables.sql
@@ -12,3 +12,20 @@ create table users
     constraint users_phone_unique unique (phone)
 );
 
+create table restaurants
+(
+    id         bigint                                  null
+        primary key,
+    name       varchar(150),
+    address    varchar(255)                            not null,
+    rating     decimal(2, 1) default 0                 null,
+    is_open    boolean       default true              null,
+    owner_id   bigint                                  null,
+    created_at timestamp     default current_timestamp null,
+    constraint restaurants_users_id_fk
+        foreign key (owner_id) references users (id)
+);
+
+alter table restaurants
+    modify name varchar(150) auto_increment;
+
