661e2a71b454936366f5b82df2735f1417cc2c1a
Akashmed <akashmed3@gmail.com>
2026-06-10 18:50:00 +0600

    create orderItem entity using JPA

---
+++ src/main/java/com/akashmed/food_delivery/entities/OrderItem.java
@@ -0,0 +1,33 @@
+package com.akashmed.food_delivery.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_item_id")
+    private MenuItem menuItem;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+}
