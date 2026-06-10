24378d648a0334a442233f711696b81b593dd1a9
Akashmed <akashmed3@gmail.com>
2026-06-10 14:41:38 +0600

    create restaurant entity using jpa

---
+++ src/main/java/com/akashmed/food_delivery/entities/Restaurant.java
@@ -0,0 +1,40 @@
+package com.akashmed.food_delivery.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "restaurants")
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "rating")
+    private BigDecimal rating;
+
+    @Column(name = "is_open")
+    private Boolean isOpen;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+}
