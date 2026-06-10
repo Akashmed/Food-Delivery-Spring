b9a963e7b532f94e931f41ddcbf77259a58206fa
Akashmed <akashmed3@gmail.com>
2026-06-10 18:49:20 +0600

    create category entity using JPA

---
+++ src/main/java/com/akashmed/food_delivery/entities/Category.java
@@ -0,0 +1,26 @@
+package com.akashmed.food_delivery.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<MenuItem> menuItems = new LinkedHashSet<>();
+
+}
