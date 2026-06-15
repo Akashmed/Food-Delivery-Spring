06d32fce83a3c287d8c414b3525799906bab6b33
Akashmed <akashmed3@gmail.com>
2026-06-16 03:26:57 +0600

    add custom validation using annotation for lowercase check

---
+++ src/main/java/com/akashmed/food_delivery/validation/LowercaseValidator.java
@@ -0,0 +1,12 @@
+package com.akashmed.food_delivery.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class LowercaseValidator implements ConstraintValidator<Lowercase, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(value == null) return true;
+        return value.equals(value.toLowerCase());
+    }
+}
