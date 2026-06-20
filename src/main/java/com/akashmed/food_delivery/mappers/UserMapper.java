30addc87d9c844f7b60433d5a4ea75253b9ee95c
Akashmed <akashmed3@gmail.com>
2026-06-21 01:12:04 +0600

    create UserMapper to convert between entity and dto

---
+++ src/main/java/com/akashmed/food_delivery/mappers/UserMapper.java
@@ -0,0 +1,13 @@
+package com.akashmed.food_delivery.mappers;
+
+import com.akashmed.food_delivery.dtos.RegisterUserRequest;
+import com.akashmed.food_delivery.dtos.UserDto;
+import com.akashmed.food_delivery.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserDto toDto (User user);
+    User toEntity (RegisterUserRequest registerUserRequest);
+}
