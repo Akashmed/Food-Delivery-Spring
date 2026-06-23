9a867bbad3c72c984866e555fbcb67c2a1d5f66b
Akashmed <akashmed3@gmail.com>
2026-06-24 03:00:14 +0600

    create UserService to implement userDetailsService

---
+++ src/main/java/com/akashmed/food_delivery/services/UserService.java
@@ -0,0 +1,28 @@
+package com.akashmed.food_delivery.services;
+
+import com.akashmed.food_delivery.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@AllArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("user not found"));
+    return new User(
+            user.getEmail(),
+            user.getPassword(),
+            Collections.emptyList()
+    );
+    }
+}
