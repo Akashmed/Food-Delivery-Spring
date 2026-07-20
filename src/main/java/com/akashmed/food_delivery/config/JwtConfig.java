082655738396544b6a49a7a5cb57fe4c8e3eabf7
Akashmed <akashmed3@gmail.com>
2026-07-20 16:15:53 +0600

    add method to get the secret key

--- src/main/java/com/akashmed/food_delivery/config/JwtConfig.java
+++ src/main/java/com/akashmed/food_delivery/config/JwtConfig.java
@@ -1,8 +1,11 @@
 package com.akashmed.food_delivery.config;
 
+import io.jsonwebtoken.security.Keys;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
 
 @Configuration
 @ConfigurationProperties(prefix = "spring.jwt")
@@ -11,5 +14,9 @@ public class JwtConfig {
     private String secret;
     private int accessTokenExpiration;
     private int refreshTokenExpiration;
+
+    public SecretKey getSecretKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
 }
